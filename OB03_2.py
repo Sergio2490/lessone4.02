9c00cfb8db9ed30a4d05501ab8f59e73c550b014
Sergio2490 <merido2490@gmail.com>
2024-11-11 00:48:07 +0300

    add all OB03_2 Agregation

--- OB03_2.py
+++ OB03_2.py
@@ -3,10 +3,13 @@ class Teacher():
     def teach(self):
         print('Преподаватель учит')
 
-class School():
-    def __init__(self):
-        pass
-    def start_lessone(self):
-        pass
+class School():   # Пропишем в def__init, что мы передаем нашего преп-ля в школу
+    def __init__(self, new_teacher): #через эту перем-ю (new_teacher) в этот класс б. передаваться инф-я об объекте класса
+        self.teacher = new_teacher  #создаем новую хар=-ку и сохр в нее нашего препод-ля, которого передали
+
+    def start_lessone(self):  # будем начинать урок
+        self.teacher.teach()  # получаем доступ к ф-ции teach( другого класса Teacher
 
 my_teacher = Teacher()
+my_school = School(my_teacher)
+my_school.start_lessone()
