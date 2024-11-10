3c637bf1c5debf684be0ae939dd478ce4bd9ec5d
Sergio2490 <merido2490@gmail.com>
2024-11-11 00:00:41 +0300

    add OB03_1 Composition

---
+++ OB03_1.py
@@ -0,0 +1,18 @@
+class Engine():
+    def start(self):
+        print("Двигатель запущен")
+
+    def stop(self):
+        print("Двигатель остановлен")
+
+class Car():
+    def __init__(self):
+        self.engine = Engine()   #внутри класса Car создали объект класса Engine -> создали строгое соответствие
+    def start(self):
+        self.engine.start()
+    def stop(self):
+        self.engine.stop()  # то же самое делаем и для ф-ции stop
+
+my_Car = Car()
+my_Car.start()
+my_Car.stop()
